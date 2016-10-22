4132b85fe80161c16cb7c6632616688f14f1fe6f
Katherine Irons <kirons323@gmail.com>
2016-10-22 14:34:06 -0400

    Update to read in initial character data
    So there's a big issue here with the GrowthRates as a nested class, I can't get the syntax working to be able to access it.  It's probably just because I'm not as familiar with classes in Python but if you can figure that out that'd be great.  I'm pretty sure it reads in the data correctly and saves it correctly, I'm just not sure how to print that part out to be 100% positive.

--- CharacterData.py
+++ CharacterData.py
@@ -8,7 +8,16 @@ class CharacterData(object):
 			a character can choose from
 		growth_rate_class: A dicitonary mapping class names (from 
 			game_clas_options) onto GrowthRates objects  
+		base_stats: A list of integers showing the character's base class stats
+		base_class: The class this character is in when they join in the game
 	"""
+	
+	name = ""
+	game_class_options = []
+	growth_rate_class = {}
+	base_stats = []
+	base_class = ""
+	
 	class GrowthRates(object):
 		"""
 		GrowthRates wrap a dictioanry of individual growth rates for a given
@@ -17,12 +26,63 @@ class CharacterData(object):
 		Attributes:
 			rates: A dictionary mapping stats onto growth rates
 		"""
+		rates = {}
 
-		def __init__(self):
+		def __init__(self, array):
 			"""Inits GrowthRates with rates"""
+			self.rates['HP'] = array[2]
+			self.rates['Str'] = array[3]
+			self.rates['Mag'] = array[4]
+			self.rates['Skl'] = array[5]
+			self.rates['Spd'] = array[6]
+			self.rates['Lck'] = array[7]
+			self.rates['Def'] = array[8]
+			self.rates['Res'] = array[9]
+			pass
+			
+		def printAllData(self):
+			for key in self.rates:
+				print(key)
+				print(self.rates[key])
 			pass
 
-	def __init__(self):
+	def __init__(self, array):
 		"""Inits CharacterData with name, game_class_options and 
 		growth_rate_class"""
+		self.name = array[0]
+		self.game_class_options = []
+		self.growth_rate_class = {}
+		self.base_stats = []
+		self.base_class = ""
+		self.addClassAndGrowthRates(array)
 		pass
+		
+	def printAllData(self):
+		print('Name: ' + self.name)
+		print('Base Class: ' + self.base_class)
+		print('Base Stats: ')
+		print(self.base_stats)
+		''' BIG PROBLEM HERE, CAN'T FIGURE OUT SYNTAX '''
+		for key in self.game_class_options:
+			print(key)
+			self.game_class_options[key].printAllData()
+		
+		print('\n')
+		pass
+		
+	def getName(self):
+		return self.name
+		
+	def setBaseClassAndStats(self, array):
+		self.base_class = array[1]
+		array[10] = array[10].strip()
+		self.base_stats = array[2:11]
+		pass	
+		
+	def addClassAndGrowthRates(self, array):
+		self.game_class_options.append(array[1])
+		gr = self.GrowthRates(array)
+		self.growth_rate_class[array[1]] = gr		
+		pass
+		
+		
