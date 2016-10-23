7d21eba384ebb8c9b2672560b090fccaa888dcbf
Katherine Irons <kirons323@gmail.com>
2016-10-23 14:22:51 -0400

    Added parameters

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import sys
 from CharacterData import CharacterData
 import Reader
+import Calculations
 
 def main():
 	f = open("./shadow_dragon.csv", "r")
-	(game_characters, max_stats, promotion_gains) = Reader.read_infile(f)
+	(game_characters, max_stats, promotion_gains, base_classes, promoted_classes) = Reader.read_infile(f)
 
 if __name__ == "__main__":
     main()
