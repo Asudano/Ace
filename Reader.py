aeacfc4d78af5c38ec4e9347d74de830c49e2c4d
Katherine Irons <kirons323@gmail.com>
2016-10-23 14:23:27 -0400

    Same sort of changes as in CharacterData, made stats clearer

--- Reader.py
+++ Reader.py
@@ -3,8 +3,10 @@ from CharacterData import CharacterData
 def read_infile(infile):
 	state = 0
 	game_characters = [] # list of characters and their game data
-	max_stats = {} # a dictionary mapping the name of a class to a list containing their maximum stats
-	promotion_gains = {} # nested dictionary {base_class: {promoted_class: stat gains/losses}}
+	max_stats = {} # nested dictionary {class: {stat name: stat}}
+	promotion_gains = {} # nested dictionaries {base_class: {promoted_class: {stat name: {stat gains/losses}}}}
+	base_classes = [] # list of base classes
+	promoted_classes = [] # list of promoted classes
 	
 	for line in infile:
 		# separators
@@ -28,7 +30,6 @@ def read_infile(infile):
 			
 		if(state == 1):
 			# start reading in characters and growth rates
-			# find character in list if it exists
 			index = -1
 			count = 0
 			for char in game_characters:
@@ -45,19 +46,37 @@ def read_infile(infile):
 		elif(state == 2):
 			# start reading in max stats for specific classes
 			class_name = array[0]
-			stat_list = array[1:9]
+			stat_list = {}
+			stat_list['HP'] = int(array[1])
+			stat_list['Str'] = int(array[2])
+			stat_list['Mag'] = int(array[3])
+			stat_list['Skl'] = int(array[4])
+			stat_list['Spd'] = int(array[5])
+			stat_list['Lck'] = int(array[6])
+			stat_list['Def'] = int(array[7])
+			stat_list['Res'] = int(array[8])
 			max_stats[class_name] = stat_list
 		elif(state == 3):
 			# start reading in promotion gains
 			base_class_name = array[0]
+			if(base_class_name not in base_classes):
+				base_classes.append(base_class_name)
 			promo_class_name = array[1]
-			stat_list = array[2:9]
+			if(promo_class_name not in promoted_classes):
+				promoted_classes.append(promo_class_name)
+			stat_list = {}
+			stat_list['HP'] = int(array[2])
+			stat_list['Str'] = int(array[3])
+			stat_list['Mag'] = int(array[4])
+			stat_list['Skl'] = int(array[5])
+			stat_list['Spd'] = int(array[6])
+			stat_list['Def'] = int(array[7])
+			stat_list['Res'] = int(array[8])
 			promo_gains = {}
 			promo_gains[promo_class_name] = stat_list
 			promotion_gains[base_class_name] = promo_gains
 		elif(state == 4):
 			# start reading in character base stats
-			# find character
 			index = -1
 			count = 0
 			for char in game_characters:
@@ -66,4 +85,4 @@ def read_infile(infile):
 				count += 1
 			game_characters[index].setBaseClassAndStats(array)
 
-	return (game_characters, max_stats, promotion_gains)
+	return (game_characters, max_stats, promotion_gains, base_classes, promoted_classes)
